7822511fb9c790751eff1081d96d36c78fca8806
Sarah Johnston <sjohnston3700@gmail.com>
2017-10-23 14:18:39 -0400

    Added jquery, changed all js to jquery. PLEASE USE JQUERY FROM NOW ON - NO MORE JAVASCRIPT. Added coding_standards.txt. Please read for standards. More will be added later.

--- python/static/js/modal.js
+++ python/static/js/modal.js
@@ -1,29 +1,20 @@
 function displayModal(modalId, btnId) {
 	
-	// Get the modal
-	var modal = document.getElementById(modalId);
+	var modal = $('#' + modalId);
+	var btn = $('#' + btnId);
 	
-	// Get the button that opens the modal
-	var btn = document.getElementById(btnId);
+	btn.click(function() {
+  		modal.css('display', 'block');
+	});
 
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
+	$('.close').click(function() {
+		modal.css('display', 'none');
+	});
 
-	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
-		modal.style.display = "block";
-	}
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-		modal.style.display = "none";
-	}
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
+	$(window).click(function(event) {
+		if (event.target.id == modalId) {
+			modal.css('display', 'none');
 		}
-	}
+	});
 }
 
