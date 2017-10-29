2c54cdc59a6d3aab6424dfea2fb3681e2e87861b
Jacob Heard <heard.jacob.7@gmail.com>
2017-10-28 21:51:27 -0400

    Added parse_grades_csv, other minor changes

--- python/Grade.py
+++ python/Grade.py
@@ -24,37 +24,4 @@ class Grade(object):
         self.public_feedback  = public_feedback
         self.private_feedback = private_feedback
     
-    
-def parse_grades( grades_text ):
-    '''
-    Function to turn a raw string of user formatted grades into a list of Grade Items
-    
-    Preconditions:
-        grades_text (str) : The raw string to parse
-    Postconditions:
-        Returns a list of Grade Objects
-        
-    Expected Format :
-    #----------------------------------------#
-    userID (int)
-    User Name
-    Feedback (multiline preformatted)
-    Total: x(int) / y(int)
-
-    #----------------------------------------#
-    Next userId etc
-    '''
-    grades = []
-    all_feedback = re.findall(FEEDBACK_PATTERN, grades_text, re.DOTALL|re.MULTILINE)
-    for feedback in all_feedback:
-        student_name = feedback[1].strip()
-        studentId = feedback[0].strip()
-        public_feedback = feedback[2]
-        grade_value = feedback[3] if feedback[3] != '' else '0'
-        out_of       = feedback[4] if feedback[4] != '' else 0 
-		
-        grade = Grade(studentId,grade_value,out_of,student_name,public_feedback)
-        grades.append(grade)
-		
-    return grades
-    
+   
