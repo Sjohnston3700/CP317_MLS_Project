78b16d0e60f3f00b4504ee89b8cc3a78e9edcee0
LucBoulanger <boul9440@mylaurier.ca>
2017-11-06 18:41:12 -0500

    added comments to Host.py

--- python/Host.py
+++ python/Host.py
@@ -5,6 +5,17 @@ class Host(object):
     Host class
     """
     def __init__(self, lms_host, protocol="http", versions=None):
+        """
+            Constructor for Host Class
+            
+            Preconditions: 
+                lms_host - hostname for the back-end LMS
+                protocol - protocol to use for user <--> web-app interaction
+                versions - Dictionary containing the latest versions of various products (needed for majority API calls)
+            
+            Postconditions:
+                returns: Object of type Host
+        """
         self._lms_host = lms_host
         self._protocol = protocol
         if versions is None:
@@ -13,10 +24,27 @@ class Host(object):
             self._versions = versions
 
     def get_protocol(self):
+        """ 
+        Getter function
+        Postconditions:
+            Returns: self._protocol - protocol used
+        """
         return self._protocol
     
     def get_lms_host(self):
+        """ 
+        Getter function
+        Postconditions:
+            Returns: self._lm_host - hostname for back-end LMS
+        """
         return self._lms_host
 
     def get_api_version(self, product_code):
+        """ 
+        Getter function
+        Preconditions:
+            product_code: String - product code
+        Postconditions:
+            Returns: LatestVersion for specific product_code
+        """
         return [item['LatestVersion'] for item in self._versions if item['ProductCode'] == product_code][0]
