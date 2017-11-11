a1a7afada5a977f50add10a4bb2c842d4fac2c45
Ben Douglas <dougers437@gmail.com>
2017-11-11 17:40:39 -0500

    fixing Grade syntax error

--- python/Grade.py
+++ python/Grade.py
@@ -7,26 +7,26 @@ FEEDBACK_PATTERN = '\-{3,}?.*?(\d{5,7}).*?^([\w|\s]*?)$(.+?(?:Total:\s+(\d+)\s+\
 class Grade(object):
     '''
     Object to hold an actual student numeric grade with a value and public/private feedback
-    
+
     '''
     '''def __init__(self,userId,value,maxValue,name='',public_feedback='',private_feedback=''):
-    
+
         Preconditions :
-            userId (str or int) : The Value userId 
+            userId (str or int) : The Value userId
             value (int)  : The grade value (asssumed numeric)
-            maxValue (int)      : What the grade  is out of 
+            maxValue (int)      : What the grade  is out of
             name (str) : The student name (optional - default = '')
             public_feedback (str) : The public feedback (optional - default = '')
             private_feedback (str) : The private feedback (optional - default = '')
-        
+
         self.userId = userId
         self.studentName = name
         self.maxValue = maxValue
         self.value  = int(value)
         self.public_feedback  = public_feedback
         self.private_feedback = private_feedback'''
-    
-    def __init__(self, grade_item, student, grade_params) {
+
+    def __init__(self, grade_item, student, grade_params):
         """
         Constructor:
             grade_item(GradeItem Object)
@@ -35,7 +35,6 @@ class Grade(object):
         self._comment = grade_params['Comments']
         self._grade_item = grade_item
         self._student = student
-    }
 
     def get_comment(self):
         """
@@ -82,10 +81,3 @@ class NumericGrade(Grade):
         Returns value of the NumericGrade Item
         """
         return self._value
-
-
-
-
-
-    
-   
