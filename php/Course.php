6f57890a2dcf1e0e7d5b42a72b21ec24dbd2b2cc
AndrewJChua <32337241+AndrewJChua@users.noreply.github.com>
2017-11-16 01:32:22 -0500

    Course.php first upload

---
+++ php/Course.php
@@ -0,0 +1,97 @@
+<?php
+
+	include 'API.php';
+	include 'GradeItem.php';
+	include 'OrgMember.php';
+
+	$GET_GRADE_ITEMS = "/d2l/api/le/(version)/(orgUnitId)/grades/";
+	$GET_MEMBERS = "/d2l/api/lp/(version)/enrollments/orgUnits/(orgUnitId)/users/"
+
+	class Course {
+		function __construct ($user,$course_params) {
+			$this->user = $user;
+			$this->name = $course_params['OrgUnit']['Name'];
+			$this->id = $course_params['OrgUnit']['Id'];
+			$this->user_role = $course_params['Access']['ClasslistRoleName'];
+			$this->grade_items = $this->_get_grade_items();
+			'self._members = [OrgMember(member) for member in API.get(GET_MEMBERS,user,{'orgUnitID':self._id})['Items']]'
+		}
+		function _get_grade_items() {
+			$gradeitems = API->get(GET_GRADE_ITEMS, self->user, array('orgUnitId'=>$this->id));
+			$items = array();
+			foreach($gradeitems as $item) {
+				if ($item['GradeType'] == 'Numeric') {
+					$items::append(NumericGradeItem($item));
+				}
+			}
+			return $items;
+		}
+		function get_grade_items() {
+			"""
+			Function will return all the current grade object (Numeric) for current course
+			return:
+            Gradeitems - list
+			"""
+			return $this->grade_items;
+		}
+		function get_grade_item(self,id) {
+			
+		}
+		function get_name() {
+			"""
+			Function will return the name of course
+			PostCondition:
+            return self.name - current course name
+			"""
+			return $this->name;
+		}
+		function get_id() {
+			"""
+			Function will return the id for the current course
+			PostCondition:
+            reutrn self.id - Id for the current course
+			"""
+			return $this->id;
+		}
+		function get_user_role() {
+			"""
+			Function will return the user fole for current course
+			PostCondition:
+            reutrn self.user_role - user role for current course
+			"""
+			return $this->user_role;
+		}
+		function get_members($role = []) {
+			if ($role != []) {
+				$items = array();
+				foreach($this->members as $member) {
+					if (in_array($member->get_user_role, $role)) {
+						$items::append($member);
+					}
+				}
+				return $items;
+			}
+			return $this->members;
+		}
+		function get_member($org_id) {
+			"""
+			Function will return the member request if it exist, 
+			Otherwise, it return None
+			"""
+			foreach ($this->members as $member) {
+				if($member->get_org_id() == $org_id) {
+					return $member;
+				}
+			}
+			return None;
+		}
+		function get_user() {
+			"""
+			Function will return the user object
+			PostCondition:
+            return self._user - user 
+			"""
+			return $this->user
+		}
+	}
+?>
