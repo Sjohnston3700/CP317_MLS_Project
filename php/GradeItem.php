479b21c6b23ff460a3497b8046e2d676f7b1755f
kostaa-k <31946393+kostaa-k@users.noreply.github.com>
2017-11-16 19:23:47 -0500

    GradeItem class numericgradeitem

--- php/GradeItem.php
+++ php/GradeItem.php
@@ -8,8 +8,8 @@ class GradeItem {
 	public function __construct($course, $grade_item_params){
 		/*
         Preconditions:
-            course (Course object) - the course the GradeItem is for
-            grade_item_params (json) - info about GradeItem  
+            $course (Course object) - the course the GradeItem is for
+            $grade_item_params (json) - info about GradeItem  
             
         Postconditions:
             parent constructor to all GradeItem types. contains data common to all types
@@ -29,7 +29,7 @@ class GradeItem {
 		/*
         Getter function
         Postconditions:
-            Returns: self._name - name of the GradeItem
+            Returns: $this->name - name of the GradeItem
         */
 		return $this->name;
 	}
@@ -38,7 +38,7 @@ class GradeItem {
         /*
         Getter function
         Postconditions:
-            Returns: self._id - Id of the GradeItem
+            Returns: $this->id - Id of the GradeItem
 		*/
         return $this->id;
     }
@@ -56,7 +56,7 @@ class GradeItem {
 		/*
         Gets grade for a given student
         Preconditions:
-            student: OrgMember -> student whose grade we are looking for
+            $student: OrgMember -> student whose grade we are looking for
         Postconditions:
             Returns: Grade object if there is a grade belonging to the student
                      Returns None if student grqade is not found
@@ -72,7 +72,7 @@ class GradeItem {
 	
     function put_grades(){
         /*
-        Calls grade.put_grade() for each Grade object
+        Calls $grade->put_grade() for each Grade object
         */
         foreach ($this->grades as $grade){
             $grade->put_grade();
@@ -102,10 +102,79 @@ class GradeItem {
         /*
         Getter function
         Postconditions:
-            Returns: self._course - Course that the GradeItem belongs to
+            Returns: $this->course - Course that the GradeItem belongs to
         */
         return $this->course;
 	}
+}
+
+class NumericGradeItem(){
+	
+	public function __construct($course, $grade_item_params){
+		/*
+        Preconditions:
+            $course (Course object) - the course the NumericGradeItem is for
+            $grade_item_params (json) - info about GradeItem (Grade.GradeObject: GradeType must be numeric!) 
+            
+        Postconditions:
+            creates object of type NumericGradeItem
+        */
+		
+		if $grade_item_params("GradeType") != "Numeric"{
+			$course_id = $course->get_id();
+			$the_grade_item = $grade_item_params("Id");
+            throw new RuntimeException("GradeType for GradeItem $course_id of Course $the_grade_item is not numeric.");
+		}
+		
+        parent::__construct($course, $grade_item_params);                    
+        $this->max_points = $grade_item_params("MaxPoints");
+        $this->can_exceed_max_points = $grade_item_params("CanExceedMaxPoints");
+	}
+	
+	function create_grade($student, $grade_params){
+        /*
+        Creates a NumericGrade object and adds to the list (Called by ezMarker)
+        Preconditions:
+            $student: OrgMember object
+            $grade_params: Python dictionary of parameters required to initialize a NumericGrade
+        */
+		$the_grade = NumericGrade($student. $grade_params);
+        $this->grades::append(NumericGrade(self, student, grade_params));
+		
+        return;
+	}
+	
+	function get_max(){
+        /*
+        Getter function
+        Postconditions:
+            Returns: $this->max_points - The max number of points this GradeItem can have for any one Grade
+        */
+        return $this->max_points;
+	}
+	
+	function get_can_exceed(){
+        /*
+        Getter function
+        Postconditions:
+            returns: $this->can_exceed_max_points
+        */
+        return $this->can_exceed_max_points;
+	}
+	
+	function within_max($value){
+        /*
+        Checks if a value is within below the max points of the grade object
+        preconditions:
+            $value: the value to check against max_points
+        Postconditions:
+            Returns: Boolean - false if not value larger than maxa points 
+        */
+        
+        return ($value <= ($this->max_points));
+	}
 	
 }
+
+
 ?>
