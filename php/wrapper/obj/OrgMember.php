f75497f8fdcfe49f873f6cd952bb9d995e0e7cfb
AndrewJChua <32337241+AndrewJChua@users.noreply.github.com>
2017-11-17 14:17:04 -0500

    Function Comments edit
    Moved php comments above the functions, as per today's meeting.

--- php/wrapper/obj/OrgMember.php
+++ php/wrapper/obj/OrgMember.php
@@ -4,61 +4,60 @@
 	include 'Course.php'; //Required in the user construct, though not present in the python version?
 	
 	class OrgMember {
+		/*
+		Instantiates a new OrgMember object 
+		Preconditions:
+			$org_member_params: Parameters to init this user (dict)
+		*/
 		function __construct($org_member_params) {
-			/*
-			Instantiates a new OrgMember object 
-			Preconditions:
-				org_member_params: Parameters to init this user (dict)
-			*/
 			$this->name = $org_member_params['User']['DisplayName'];
 			$this->id = $org_member_params['User']['Identifier'];
 			$this->org_id = $org_member_params['User']['OrgDefinedId'];
 			$this->role = $org_member_params['Role']['Id'];
 		}
-		
+		/*
+		Postconditions:
+			returns
+			The name of the OrgMember (str)
+		*/
 		function get_name() {
-			/*
-			Postconditions:
-				returns
-				The name of the OrgMember (str)
-			*/
 			return $this->name;
 		}
+		/*
+		Postconditions:
+			returns
+			The Brightspace ID of the OrgMember (str)
+		*/
 		function get_id() {
-			/*
-			Postconditions:
-				returns
-				The Brightspace ID of the OrgMember (str)
-			*/
 			return $this->id;
 		}
+		/*
+		Postconditions:
+			returns
+			The organization defined ID of the OrgMember (str)
+		*/
 		function get_org_id() {
-			/*
-			Postconditions:
-				returns
-				The organization defined ID of the OrgMember (str)
-			*/
 			return $this->org_id;
 		}
+		/*
+		Postconditions:
+			returns
+			The role id of the OrgMember (str)
+		*/
 		function get_role() {
-			/*
-			Postconditions:
-				returns
-				The role id of the OrgMember (str)
-			*/
 			return $this->role;
 		}
 	}
 	
 	class User {
+		/*
+		Instantiates a new User object
+		Preconditions:
+			$context: The Brightspace User context ()
+			$host: The Host object corresponding to the user (Host)
+			$roles: List of roles, default: None (list)
+		*/
 		function __construct($context, $host, $roles=[]) {
-			/*
-			Instantiates a new User object
-			Preconditions:
-				context: The Brightspace User context ()
-				host: The Host object corresponding to the user (Host)
-				roles: List of roles, default: None (list)
-			*/
 			$this->context = $context;
 			$this->host = $host;
 			$me = API->get_who_am_i(host);
@@ -71,33 +70,29 @@
 					$this->courses = new Course($item);
 				}
 			}
-			//TO EDIT
 		}
-		function get_courses() {
-			/*
-			Returns a copy of the list of courses the user has access to
-		
-			Postconditions:
-				returns
-				Copy of a python list of all courses accessible by this user
+		/*
+		Returns a copy of the list of courses the user has access to
+		Postconditions:
+			returns
+			Copy of a python list of all courses accessible by this user
 			*/
+		function get_courses() {
 			$copy = array();
 			foreach($this->courses as $i => $j) {
 				$copy[$i] = clone $v;
 			}
 			return $copy;
 		}
+		/*
+		Returns a single course with id matching the given id, None if this User does not have access to the course
+		Preconditions:
+			id - ID of the course (str or int)
+		Postconditions
+			returns
+			A single course object with matching id, None if this User cannot access that course
+		*/
 		function get_course($id) {
-			/*
-			Returns a single course with id matching the given id, None if this 
-			User does not have access to the course
-		
-			Preconditions:
-				id - ID of the course (str or int)
-			Postconditions
-				returns
-				A single course object with matching id, None if this User cannot access that course
-			*/
 			foreach($this->courses as $course) {
 				if($course->get_id == $id) {
 					return $course;
@@ -105,24 +100,22 @@
 			}
 			return NULL;
 		}
+		/*
+		Returns the user context belonging to this user
+		Postconditions:
+			returns
+			A Brightspace user context
+		*/
 		function get_context() {
-			/*
-			Returns the user context belonging to this user
-		
-			Postconditions:
-				returns
-				A Brightspace user context
-			*/
 			return $this->context;
 		}
+		/*
+		Gets the host being used by this User
+		Postconditions:
+			returns 
+			A Host object
+		*/
 		function get_host() {
-			/*
-			Gets the host being used by this User
-		
-			Postconditions:
-				returns 
-				A Host object
-			*/
 			return $this->host;
 		}
 	}
