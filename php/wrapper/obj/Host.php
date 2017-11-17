18bb1ca772750444f7b198b784a012aea8194ec3
malthibeh <alth5240@mylaurier.ca>
2017-11-17 15:15:19 -0500

    Comments
    docstring edited

--- php/wrapper/obj/Host.php
+++ php/wrapper/obj/Host.php
@@ -4,19 +4,19 @@ include ("API.php");
 
 class Host{ 
 
+	/*
+	Constructor for Host Class
+	
+	Preconditions: 
+		lms_host - hostname for the back-end LMS
+		protocol - protocol to use for user <--> web-app interaction
+		versions - Dictionary containing the latest versions of various products (needed for majority API calls)
+	
+	Postconditions:
+		returns: Object of type Host
+	*/
 	function __construct($lms_host, $protocol="http", $versions=NULL) {
-		/*
-		Constructor for Host Class
-		
-		Preconditions: 
-			lms_host - hostname for the back-end LMS
-			protocol - protocol to use for user <--> web-app interaction
-			versions - Dictionary containing the latest versions of various products (needed for majority API calls)
-		
-		Postconditions:
-			returns: Object of type Host
-        */
-		
+
 		$this->_lms_host = $lms_host;
 		$this->_protocol = $protocol;
 
@@ -28,34 +28,38 @@ class Host{
 
    }
    
+   /* 
+	Getter function
+	Postconditions:
+		Returns: self._protocol - protocol used
+	*/
    function get_protocol() {
-	   /* 
-        Getter function
-        Postconditions:
-            Returns: self._protocol - protocol used
-        */
 
         return $this->_protocol;
     }  
 	
+	
+	/*
+	Getter function
+	Postconditions:
+		Returns: self._lm_host - hostname for back-end LMS
+	*/
 	function get_lms_host() { 
-		/*
-		Getter function
-		Postconditions:
-			Returns: self._lm_host - hostname for back-end LMS
-		*/
 
         return $this->_lms_host;
-    }  
+    }
+
+
+	/* 
+	Getter function
+	Preconditions:
+		product_code: String - product code
+	Postconditions:
+		Returns: LatestVersion for specific product_code
+	*/
+			
 	function get_api_version($product_code) {
-		/* 
-        Getter function
-        Preconditions:
-            product_code: String - product code
-        Postconditions:
-            Returns: LatestVersion for specific product_code
-        */
-		
+
         return [$item['LatestVersion'] foreach $this->versions as $item if($item['ProductCode'] == $product_code)][0];
     }  
 	
