9248daa15bb0ce27789691cba361d18233296136
shuaibr <31935226+shuaibr@users.noreply.github.com>
2017-11-18 12:34:07 -0500

    Unit testing from PHPUnit, stack and user test
    Created tests that check proper function of array and creation of new user

--- php/unit_testing/UserTest.php
+++ php/unit_testing/UserTest.php
@@ -3,10 +3,46 @@
 
 use PHPUnit\Framework\TestCase;
 
+class StackTest extends TestCase 
+
+{ 
+	//Assert array as empty 
+	public function testEmpty()
+	{
+		$stack = [];
+		$this->asserEmpty($stack); 
+		
+		return $stack; 
+		
+	}
+	
+	//Test push and pop 
+	public function testPushandPop()
+	
+	{
+		$stack = [];
+		$this->assertEquals(0,count($stack));
+		
+		array_push($stack, 'foo');
+		$this->assertEquals('foo',$stack[count($stack)-1]);
+		$this->assertEquals(1, count($stack));
+		
+		$this->assertEquals('foo', array_pop($stack));
+		$this->assertEquals(0, count($stack));
+		
+	}
+}
+
+
 final class UserTest extends TestCase
 {
     public function testCreateUser() {
-        
+		
+		$user = user('student')->create();
+		
+        $this->assertTrue(true);
+		
+
     }
 
 }
