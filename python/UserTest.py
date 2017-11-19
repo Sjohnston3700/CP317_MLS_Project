dc5b15d4d73ed04d85a39372566600f6d09f1f70
Sumeet Jhand <sammy_634@hotmail.com>
2017-11-18 22:53:11 -0500

    Fixed unit testing base class. Should work fine now.

--- python/UserTest.py
+++ python/UserTest.py
@@ -1,26 +1,13 @@
-"""
-Tests User class
-Requires that Host, API file work
-start() and auth_token_handler() needed to get user context
-
-Click the url that appears in the console, fill in user credentials
-Can debug in Web debugger, just go to last couple lines to see what caused error
-and then can click terminal icon on right side query for object data
-
-Nothing will work until various bugs get fixed. I (Troy) have fixed some already, but stopping because not my role
-
-Version: Nov. 16, 2017
-"""
-
-
-import unittest # need, even though says import not used
 from OrgMember import User
-import requests # may not need. Delete if so
 import d2lvalence.auth as d2lauth
+import sys
 
+from threading import Thread, Event
+import logging
 from flask import Flask, redirect, request
 from conf_basic import app_config
 from Host import Host
+import unittest
 
 LOGOUT_URL = 'https://{host}/d2l/logout'
 
@@ -36,29 +23,44 @@ def start():
 
 @app.route(app_config["route"])
 def auth_token_handler():
-    uc = app.config["app_context"].create_user_context( result_uri=request.url, host=app_config['lms_host'], encrypt_requests=app_config['encrypt_requests'])
+    print("Authentication successful.")
+    
+    uc = app.config["app_context"].create_user_context(result_uri=request.url, host=app_config['lms_host'], encrypt_requests=app_config['encrypt_requests'])
     host = Host(app_config['lms_host'])
     # store the user context's
     user = User(uc, host)
     app.config['user'] = user
-
-# add tests to this class for user
-# need to make it so that messages are displayed for tests
-class test_user(unittest.TestCase):
-
-    # runs start(), which then calls auth_token_handler()
-    # user object will be stored in app.config after
-    # could try changing to set_up to match standard, but not sure if unittest will handle that
-    def setUp(self):
-        app.run(port=8080,debug=True)
-
-    def test_who_am_i(self):
-        print(app.config['user'].get_name())
     
-    #should probably put logout here
+    print("Starting test thread...")
+    
+    # creates and starts a new thread for the unit tests to run in
+    thread = Thread(target=unittest.main, args=())
+    thread.start()
+    
+    # waits for the testing to finish
+    thread.join()
+    
+    sys.exit(0)    
+
+# unit testing class
+# do not change setUp and tearDown to snake case, it'll break
+class test_user(unittest.TestCase):
+    # stores the user object before every test
+    def setUp(self):
+        self.user = app.config['user']
+
+    # prints the user's name
+    def test_who_am_i(self):
+        print(self.user.get_name())
+    
     def tearDown(self):
         return
 
 # says that if running this file, call unittest.main (which calls setUp)    
 if __name__ == "__main__":
-    unittest.main()
+    # gets rid of Flask's HTTP request logging
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+    
+    print("Go to {0}://{1}:{2} to authenticate.".format(app_config['scheme'], app_config['host'], app_config['port']))
+    app.run(port=8080,debug=False)
