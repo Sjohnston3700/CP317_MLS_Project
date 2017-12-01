c9cb36caa686d722a93346555930bce185e70788
Lucas Boulanger <boul9440@mylaurier.ca>
2017-11-30 20:25:03 -0500

    removed old functions and fixed spelling error in grade_functions.py

--- python/grade_functions.py
+++ python/grade_functions.py
@@ -1,81 +1,11 @@
 import csv # To handle csv files
 import re,sys
 
-
 from wrapper.obj.API import get as getRoute, put as putRoute
 from wrapper.obj.Grade import Grade
 from io import TextIOWrapper # To check if file object is valid
 
-
-gradeItemsRoute = '/d2l/api/le/(version)/(orgUnitId)/grades/'
-gradeItemRoute  = '/d2l/api/le/(version)/(orgUnitId)/grades/(gradeObjectId)'
-setGradeRoute   = '/d2l/api/le/(version)/(orgUnitId)/grades/(gradeObjectId)/values/(userId)'
-gradeItemKeys = ['Name','MaxPoints','Id']#List of items to extract from each grade item
-grade_json = {"Comments": { "Content": "Test comment", "Type": "Text" }, "PrivateComments": { "Content": "Test Private Comment", "Type": "Text" }, "GradeObjectType": 1, "PointsNumerator": 23}
-
-def getGradeItems(uc,courseId,apiVersion=1.22):
-    '''
-    Function to return a list of grade items in the course identified by courseID
-    Fails  with an RuntimeError Exception if something goes wrong
-
-    Preconditions:
-    uc (api user context) : The api user context to make the call with
-    courseId (str or int) : The Id of the course we're getting grade items from
-    apiVersion (float) : The api Version to use (default = 1.22)
-    
-    Postconditions:
-    Returns a list of grade items associated with this course. 
-    
-    '''
-    results = None
-    r = getRoute(uc, gradeItemsRoute,{'version':apiVersion,'orgUnitId':courseId})
-
-    if r.status_code != 200:
-        raise RuntimeError('Failed to get grade items. Request returned status code : {}, text : {}'.format(r.status_code,r.text) )
-    else:
-        results = []
-        for result in r.json():
-            grade = {}
-            for key in gradeItemKeys:
-                grade[key] = result[key]
-            results.append(grade)
-    return results
-
-
-def setGradeItems(uc,courseId,gradeId,newGrades,apiVersion=1.22):
-    '''
-    Function to set the grades for a specific grade item. Assumes they are numeric grades
-    Throws a RuntimeError Exception if something goes wrong.
-
-    Preconditions:
-    uc (api user context) : The api user context to make the call with
-    courseId (str or int) : The Id of the course we're setting the grade item for
-    gradeId (str or int)  : The Id of the grade Item we're setting
-    newGrades ( list of dictionary itens ) : The new grades to set (eg. [{'ValenceId':studentValenceId,'Grade':gradevalue,'Comments':student_feedback,'PrivateComments':optionalFeedbackForMarkers}]
-    apiVersion (float) : The api Version to use (default = 1.22)
-    
-    Postconditions:
-    Returns status code
-    '''
-    
-    #Get grade item to make sure the values are correct
-    grade_request = getRoute(uc, gradeItemRoute,{'version':apiVersion,'orgUnitId':courseId,'gradeObjectId':gradeId})
-    if grade_request.status_code != 200:
-        raise RuntimeError('Failed to get grade item. Request returned status code : {}, text : {}'.format(r.status_code,r.text) )
-    else:
-        for new_grade in newGrades:
-            gradeInfo = grade_json
-            gradeInfo["Comments"]["Content"]        = new_grade["Comments"]
-            gradeInfo['PrivateComments']["Content"] = new_grade['PrivateComments'] if 'PrivateComments' in new_grade else ""
-            gradeInfo["PointsNumerator"] = new_grade["Grade"]
-
-            grade_put = putRoute(uc, setGradeRoute,{'version':apiVersion,'orgUnitId':courseId,'gradeObjectId':gradeId,'userId':new_grade["ValenceId"]},gradeInfo )
-            if grade_put.status_code != 200:
-                raise RuntimeError('Failed to set grade for {}. Request returned status code : {}, text : {}'.format(new_grade["ValenceId"],grade_put.status_code,grade_put.text) )
-    
-    return
-
-def parse_grades( grades_text ):
+-def parse_grades( grades_text ):
     '''
     Function to turn a raw string of user formatted grades into a list of Grade Items
     
@@ -108,7 +38,6 @@ def parse_grades( grades_text ):
         
     return grades
 
-
 def parse_grades_csv( csv_file ):
     '''
     Parses Grade objects from a CSV file in the format
@@ -129,7 +58,7 @@ def parse_grades_csv( csv_file ):
         for line_number, line in enumerate(reader):
             # unescape any escaped characters read by csv.reader
             if len(line) < 4:
-                errors.apppend( {'line':line_number,'msg':'Format must be brightspace_id, grade, student_name, comment'} )
+                errors.append( {'line':line_number+1,'msg':'Format must be brightspace_id, grade, student_name, comment'} )
             else:
                 comment = line[3]
                 for before, after in {'\\n':'\n', '\\r':'\r', '\\t':'\t', '\\"': '"', "\\'":"'"}:
