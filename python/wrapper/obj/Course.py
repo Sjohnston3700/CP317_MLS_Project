0f160434592010caa8dd92d6c25d8aab751c5bec
Lucas Boulanger <boul9440@mylaurier.ca>
2017-12-03 21:22:38 -0500

    Fixed bug where org defined id was being compared to Brightspace identifier, giving a student not found error when inputting grades manually -> added OrgMember.get_Identifier()

--- python/wrapper/obj/Course.py
+++ python/wrapper/obj/Course.py
@@ -126,7 +126,7 @@ class Course(object):
                 None
         """
         for member in self._members:
-            if str(member.get_org_id()) == str(org_id):
+            if str(member.get_identifier()) == str(org_id):
                 return member
         return None        
 
