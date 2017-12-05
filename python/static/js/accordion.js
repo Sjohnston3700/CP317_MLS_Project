3f2dd68efb5a893638aaf0a6b1d72f7a82a613fd
Sarah Johnston <sjohnston3700@gmail.com>
2017-12-05 12:09:40 -0500

    Changed accordion.js back to original, cleaned up css, made transition on open/close again

--- python/static/js/accordion.js
+++ python/static/js/accordion.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
 
-$(document).on('click', '.course-item > a', function(e){
-	e.preventDefault();
-		var trigger = $(this);
-		var course = trigger.parents('.course-item');
-		var grades = course.find('.course-grades');
+	var acc = $('.accordion');
+	var i;
 
-		if(course.hasClass('active')) {
-			course.removeClass('active');
-		} else {
-			course.addClass('active');
+	for (i = 0; i < acc.length; i++) {
+			acc[i].onclick = function() {
+			this.classList.toggle('active');
+			var panel = this.nextElementSibling;
+			panel.classList.toggle('open');
+			if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+			} 
+			else {
+				var height = parseInt(panel.scrollHeight) + 30;
+				panel.style.maxHeight = height + "px";
+			}
 		}
-	});
-});
+	}
+
+}); 
