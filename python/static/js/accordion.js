38fd05858926103f5d5a0fb3c455ed5aa367de3d
LucBoulanger <boul9440@mylaurier.ca>
2017-12-08 21:39:16 -0500

    added back stuff to accordion.js so python doesn't get confused again.

--- python/static/js/accordion.js
+++ python/static/js/accordion.js
@@ -1,5 +1,17 @@
-$(document).ready(function() {
+$(document).on('click', '.course-item > a', function(e){
+	e.preventDefault();
+		var trigger = $(this);
+		var course = trigger.parents('.course-item');
+		var grades = course.find('.course-grades');
 
+		if(course.hasClass('active')) {
+			course.removeClass('active');
+		} else {
+			course.addClass('active');
+		}
+});
+    
+$(document).ready(function() {
 	var acc = $('.accordion');
 	var i;
 
@@ -17,5 +29,4 @@ $(document).ready(function() {
 			}
 		}
 	}
-
 }); 
