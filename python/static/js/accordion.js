a70fb27ca2fadec238f25d5fce52c13857291963
LucBoulanger <boul9440@mylaurier.ca>
2017-12-09 14:14:18 -0500

    Reverted additions made yesterday, made python course page look same as a PHP course page.

--- python/static/js/accordion.js
+++ python/static/js/accordion.js
@@ -1,16 +1,3 @@
-$(document).on('click', '.course-item > a', function(e){
-	e.preventDefault();
-		var trigger = $(this);
-		var course = trigger.parents('.course-item');
-		var grades = course.find('.course-grades');
-
-		if(course.hasClass('active')) {
-			course.removeClass('active');
-		} else {
-			course.addClass('active');
-		}
-});
-    
 $(document).ready(function() {
 	var acc = $('.accordion');
 	var i;
