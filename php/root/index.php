a4384e1006de86bbe40cb6a06f3d5f3be40f25bd
malthibeh <alth5240@mylaurier.ca>
2017-12-10 15:39:34 -0500

    Made logo Clickable
    Made logo Clickable to home

--- php/root/index.php
+++ php/root/index.php
@@ -135,7 +135,7 @@
 	</head>
 	<body>
 		<ul class="horiz-nav">
-			<img id="logo" src="<?=$PATH_TO_STATIC?>/img/logo.png">
+			<a href="index.php?page=home"><img id="logo" src="<?=$PATH_TO_STATIC?>/img/logo.png" ></a>
 			<li class="brand">
 				<a href="index.php?page=home">
 					ezMarker
