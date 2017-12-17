1ca3a6e7673ca3f9b57202bb576d1b7b88e38f12
Troy Nechanicky <nech5860@mylaurier.ca>
2017-12-16 23:19:07 -0500

    PHP: courses page now shows loading animation.

---
+++ php/root/actions/get_courses.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once('../../wrapper/obj/User.php');
+
+session_start();
+
+// If no course or user tokens set, the user is doing something suspicious, so don't continue
+if (!isset($_SESSION['user']))
+{
+	die();
+}
+
+$user = unserialize($_SESSION['user']);
+$courses = array();
+
+foreach($user->get_courses() as $course) {
+    $data = array(
+        'name' => $course->get_name(),
+        'id' => $course->get_id(),
+        'grade_items' => array()
+    );
+
+    foreach($course->get_grade_items() as $grade_item) {
+        array_push($data['grade_items'], array(
+            'name' => $grade_item->get_name(),
+            'id' => $grade_item->get_id()
+        ));
+    }
+    array_push($courses, $data);
+}
+
+echo json_encode($courses);
+
+?>
+
--- php/views/courses.php
+++ php/views/courses.php
@@ -1,25 +1,15 @@
-<?php 
+<div id="available-courses"></div>
 
-$courses = $user->get_courses();
+<!--Loading animation-->
+<div class="loader-box hidden">
+	<div class="loader-msg">Getting Available Courses</div>
+	<div class="loader"></div> 
+</div>
+<!-- Template HTML -->
+<div class="templates">
+	<button class="accordion accordion-template hidden"></button>
+	<div class="panel panel-template hidden"></div>
+</div>
 
-?>
-
-<?php if (empty($courses)) { ?>
-	<p>You do not have permission to set grades for any courses</p>
-	<?php exit; ?>
-<?php } ?>
-
-
-<h2>Select a Grade Item</h2>
-<?php foreach ($courses as $c) { ?>
-	<button class="accordion"><?=$c->get_name()?></button>
-	<div class="panel">
-		<?php if (sizeof($c->get_grade_items()) > 0) { ?>
-			<?php foreach ($c->get_grade_items() as $g) { ?>
-			<a class="grade-link" href="index.php?page=upload&course=<?=$c->get_id()?>&grade_item=<?=$g->get_id();?>"><?=$g->get_name()?></a>
-			<?php } ?>
-		<?php } else { ?>
-			<p> No grade items for this course</p>
-		<?php } ?>
-	</div>
-<?php } ?>
+<script type="text/javascript" src="<?=$PATH_TO_STATIC?>/js/accordion.js"></script>
+<script type="text/javascript" src="<?=$PATH_TO_STATIC?>/js/courses.js"></script>
